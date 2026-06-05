740602f61e3a3f4a706136597835f604486c4e54
Bryan <brayenangelo7@gmail.com>
2026-06-05 10:26:20 +0700

    fix database

--- DatabaseSQL/QueryInsert.sql
+++ DatabaseSQL/QueryInsert.sql
@@ -6,7 +6,7 @@ INSERT INTO Guru (id_guru, nama, email, no_hp, password) VALUES (2, 'Guru 2', '-
 INSERT INTO Guru (id_guru, nama, email, no_hp, password) VALUES (3, 'Guru 3', '-', '-', '-');
 
 -- 2. Masukkan Jadwal Bohongan agar sama dengan React
-INSERT INTO Jadwal_Kesediaan_Guru (id_jadwal, id_guru, hari, jam_mulai, jam_selesai, status, id_admin)
+INSERT INTO Jadwal_Kesediaan_Guru (id_jadwal, id_guru, hari, jam_mulai, jam_selesai, id_admin)
 VALUES 
 (101, 1, 'Monday', '14:00:00', '15:00:00', 1),
 (102, 1, 'Monday', '15:00:00', '16:00:00', 1),
